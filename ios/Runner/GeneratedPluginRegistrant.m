f8bc3505c8093993f3decc93db25c355c94dd340
Yashique Chalil <yashiquechalil@gmail.com>
2021-08-05 15:25:29 +0530

    2nd commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_nfc_reader/FlutterNfcReaderPlugin.h>)
+#import <flutter_nfc_reader/FlutterNfcReaderPlugin.h>
+#else
+@import flutter_nfc_reader;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterNfcReaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterNfcReaderPlugin"]];
+}
+
+@end
